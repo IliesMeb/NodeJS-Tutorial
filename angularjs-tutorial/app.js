9534377c6f4bcafbd3475a9bb9d770ec0073c5a3
Ilies Mebarki <Ilies.mebarki@googlemail.com>
2021-04-17 17:10:09 +0200

    part 3 finished

--- angularjs-tutorial/app.js
+++ angularjs-tutorial/app.js
@@ -1,6 +1,14 @@
 var app = angular.module("tutorialApp", []);
 
-// $scope übertragt Daten vom Controller an den View bzw. andersrum
-app.controller("tutorialController", function($scope) {
-    $scope.title = "Welt";
+// $scope übertragt Daten vom Controller an den View bzw. auch andersrum
+app.controller("tutorialController", function($scope, $timeout) {
+    $scope.counter = 0;
+
+    $scope.incrementCounter = function() {
+        $timeout(function() {
+            $scope.counter = $scope.counter + 1;
+            console.log($scope);
+        }, 1000);
+        // check for scope changes
+    };
 });
